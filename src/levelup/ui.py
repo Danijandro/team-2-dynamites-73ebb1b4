95cb349ccf17cf67aa14df1e623b2aeacfc9b9d3
Danijandro <daniel.lingerfelt@gmail.com>
2023-10-26 17:43:58 -0400

    Added screen #1 and changed screen #2

--- src/levelup/ui.py
+++ src/levelup/ui.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 from typing import Callable
 from levelup.controller import GameController, Direction, InvalidMoveException
 
@@ -18,8 +19,20 @@ class GameApp:
                 break
         return response
 
+    def game_intro(self):
+        while True:
+            response = input("\n\n\n\n\n\n\nWelcome to the Legendary Dynamites Journey" +
+                "\n\nYou will embark on a journey through a jungle filled with tigers, snakes, and dangerous obstacles," +
+                "\nwith the goal of finding a golden treasure." +
+                "\n\nAre you ready to begin? (y/n)")
+            if response in ["n","N"]:
+                print ("Hope to see you again soon. Thanks!\n\n")
+                sys.exit()
+            elif response in ["y","Y"]:
+                break
+
     def create_character(self):
-        character = self.prompt("Enter character name", lambda x: len(x) > 0)
+        character = self.prompt("What is your name?", lambda x: len(x) > 0)
         self.controller.create_character(character)
 
     def move_loop(self):
@@ -38,9 +51,11 @@ class GameApp:
             print(self.controller.status)
 
     def start(self):
+        self.game_intro()
         self.create_character()
         self.controller.start_game()
         self.move_loop()
 
     def quit(self):
         print(f"\n\n{self.controller.status}")
+
